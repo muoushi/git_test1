18b043d9dbb0f8dae0effdfb973e7535a6b188c6
linxinhui <www.1035339453.com@qq.com>
2020-03-25 12:39:11 +0800

    工程添加到本地仓库中

---
+++ git_test/src/com/itheima/git/firstClass.java
@@ -0,0 +1,21 @@
+package com.itheima.git;
+
+/**
+ * @author ：X H
+ * @version :
+ * @date ：Created in 2020/3/25 12:29
+ * @description：
+ * @modified By：
+ */
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+
+    }
+}
